1a8433ca06c45b82f5f0d83b29fcde1f0677d321
Alex <friedmanalexg@gmail.com>
2022-12-04 13:06:09 -0500

    mastofeed kinda almost works

--- client/src/components/home/feed/Feed.jsx
+++ client/src/components/home/feed/Feed.jsx
@@ -6,9 +6,7 @@ const Feed = () => {
       <p className="text-3xl text-gray-700 font-bold mb-5">
         Bot Feed
       </p>
-      <p className="text-gray-500 text-lg">
-        bot feed go here
-      </p>
+      <iframe allowfullscreen sandbox="allow-top-navigation allow-scripts" width="400" height="800" src="https://www.mastofeed.com/apiv2/feed?userurl=https%3A%2F%2Fbotsin.space%2Fusers%2Fteedoff&theme=dark&size=100&header=true&replies=false&boosts=false"></iframe>
     </div>
   )
 }
