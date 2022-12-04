c914d2fea3d77f8a24c5373b5f47b220109fc3fd
AndreaRose92 <104471962+AndreaRose92@users.noreply.github.com>
2022-12-04 07:26:07 -0800

    Update golf_course.rb

--- app/models/golf_course.rb
+++ app/models/golf_course.rb
@@ -1,19 +1,3 @@
 class GolfCourse < ApplicationRecord
   belongs_to :city
-
-  def crunch_numbers
-    option = rand(1..5)
-    case option
-    when 1
-      output = "#{((self.water_usage.to_i * 7) / 660000)} olympic swimming pools"
-    when 2
-      output = "#{((self.water_usage.to_i * 7) / 80)} bathtubs"
-    when 3
-      output = "#{((self.water_usage.to_i * 7) / 40000000)} days of US coffee consumption"
-    when 4
-      output = "#{((self.water_usage.to_i * 7) / 1000000)} water towers"
-    when 5
-      output = "#{((self.water_usage.to_i * 7) / 113)} years of coffee for the average American"
-    end
-  end
 end
